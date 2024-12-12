af72c6f3a92978ec3173643e9a3556d2ac143538
Richard <dygogive@gmail.com>
2024-12-12 11:07:36 +0200

    Initial commit

--- app/src/main/java/com/example/gymlog/Exercise.java
+++ app/src/main/java/com/example/gymlog/Exercise.java
@@ -7,7 +7,7 @@ import androidx.annotation.NonNull;
 import java.util.ArrayList;
 import java.util.List;
 
-//test comit user1
+//test comit master
 public class Exercise {
 
     private String name;
