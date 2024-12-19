7cfbf3febfb01ade3c56ef824cffd91c06a2de5e
Richard <dygogive@gmail.com>
2024-12-19 23:36:10 +0200

    Initial commit

--- app/src/main/java/com/example/gymlog/data/exercise/Motion.java
+++ app/src/main/java/com/example/gymlog/data/exercise/Motion.java
@@ -7,8 +7,18 @@ import com.example.gymlog.R;
 public enum Motion {
 
     //ОСНОВНІ РУХИ ПРИ ВИКОНАННІ ВПРАВ
-    PRESS_BY_ARMS(R.string.motion_press_by_arms),
-    PULL_BY_ARMS(R.string.motion_pull_by_arms),
+    PRESS_UPWARDS(R.string.motion_press_upwards),
+    PRESS_DOWNWARDS(R.string.motion_press_downwards),
+    PRESS_MIDDLE(R.string.motion_press_middle),
+    PRESS_UP_MIDDLE(R.string.motion_press_up_middle),
+    PRESS_DOWN_MIDDLE(R.string.motion_press_down_middle),
+
+    PULL_UPWARDS(R.string.motion_pull_upwards),
+    PULL_DOWNWARDS(R.string.motion_pull_downwards),
+    PULL_MIDDLE(R.string.motion_pull_middle),
+    PULL_UP_MIDDLE(R.string.motion_pull_up_middle),
+    PULL_DOWN_MIDDLE(R.string.motion_pull_down_middle),
+
     PRESS_BY_LEGS(R.string.motion_press_by_legs),
     PULL_BY_LEGS(R.string.motion_pull_by_legs);
 
