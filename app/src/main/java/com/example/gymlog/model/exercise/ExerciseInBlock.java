debbe9e347f4b1b2b8a0d428facd406e860d7824
Richard <dygogive@gmail.com>
2025-03-28 01:28:41 +0200

    works...

--- app/src/main/java/com/example/gymlog/model/exercise/ExerciseInBlock.java
+++ app/src/main/java/com/example/gymlog/model/exercise/ExerciseInBlock.java
@@ -1,12 +1,10 @@
 package com.example.gymlog.model.exercise;
 
-import android.content.Context;
-
 import java.util.List;
 
 //вправи, що в списках для тренувальних блоків
 public class ExerciseInBlock extends Exercise {
-    private long linkId; // id з таблиці TrainingBlockExercises
+    private final long linkId; // id з таблиці TrainingBlockExercises
     private int position;
 
     public ExerciseInBlock(long linkId, long exerciseId, String name, String description, Motion motion, List<MuscleGroup> muscleGroups, Equipment equipment, int position) {
