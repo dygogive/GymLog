f39f33443e440555e92f052678edde5dea3b77fc
dygogive <dygogive@gmail.com>
2025-03-28 13:15:01 +0200

    test from pc 28mar2025

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ android {
 
     defaultConfig {
         applicationId = "com.example.gymlog"
-        minSdk = 33
-        versionCode = 2
-        versionName = "1.1"
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        signingConfig = signingConfigs.getByName("debug")
+        minSdk = 24
         targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
