b2e60417a28106fa5afe98701720e0feed37ebdc
Richard <dygogive@gmail.com>
2025-05-17 21:14:49 +0300

    First working MVP version of GymLog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.example.gymlog.test"
+        applicationId = "com.example.gymlog"
         minSdk = 28
         targetSdk = 36
         versionCode = 1
